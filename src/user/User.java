c52db9c23b289532747e40f933ff17c2c735dd04
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-09-23 21:23:23 +0530

    add createPassword() method that returns a customized password

--- src/user/User.java
+++ src/user/User.java
@@ -1,5 +1,7 @@
 package user;
 
+import java.util.Random;
+
 public class User {
     private String name;
     private String companyName;
@@ -58,6 +60,32 @@ public class User {
     public String createEmail() {
         return String.format("%s_%d@%s.com", getName().toLowerCase(), getAge(), getCompanyName().toLowerCase());
     }
+
+    public String createPassword() {
+        Random random = new Random();
+
+        int randomNumber1 = random.nextInt(9);
+        int randomNumber2 = random.nextInt(9);
+        int randomNumber3 = random.nextInt(9);
+        int randomNumber4 = random.nextInt(9);
+
+        String alphabets = "abcdefghijklmnopqrstuvwxyz";
+        char randomAlphabet = alphabets.charAt(random.nextInt(alphabets.length()));
+
+        String specialCharacters = "!@#$%&*";
+        char randomSpecialCharacter = specialCharacters.charAt(random.nextInt(specialCharacters.length()));
+
+        String first5CharOfName;
+        if (name.length() < 5) {
+            first5CharOfName = (name + "00000").substring(0, 5);
+        } else {
+            first5CharOfName = name.substring(0, 5);
+        }
+
+        return String.format("%c%d%d%d%d%s%s", randomAlphabet, randomNumber1, randomNumber2, randomNumber3, randomNumber4, first5CharOfName, randomSpecialCharacter);
+
+    }
+
 
 
 
