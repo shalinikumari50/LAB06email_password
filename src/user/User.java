9d518f057c684f54d6c5fe7b9ec1ca737413398e
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-09-23 20:25:26 +0530

    generate getters and setters for all variables

--- src/user/User.java
+++ src/user/User.java
@@ -7,4 +7,43 @@ public class User {
     private String email;
     private String password;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
