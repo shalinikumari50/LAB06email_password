98ba66acddd15869a82ec66ee270296fd4e78246
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-09-23 21:30:51 +0530

    print "N/A" if numberOfEntries==0

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,6 +6,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int numberOfEntries = Integer.parseInt(scanner.nextLine());
+        if (numberOfEntries == 0) {
+            System.out.println("N/A");
+        }
+
 
         scanner.close();
     }
